643529b7e23260c92fbd0d0a4f41b7eb0457a95a
pratik3615 <94849401+pratik3615@users.noreply.github.com>
2024-08-02 20:30:40 +0530

    DB Updated

--- DataBase/LoginDb.sql
+++ DataBase/LoginDb.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `etmsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `etmsystem`;
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: etmsystem
 -- ------------------------------------------------------
--- Server version	8.0.37
+-- Server version	8.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +16,54 @@ USE `etmsystem`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client` (
+  `id` int NOT NULL,
+  `email_id` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `contact_no` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `designation`
+--
+
+DROP TABLE IF EXISTS `designation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `designation` (
+  `desig_id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`desig_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `designation`
+--
+
+LOCK TABLES `designation` WRITE;
+/*!40000 ALTER TABLE `designation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `designation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `employee`
 --
 
@@ -25,16 +71,19 @@ DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employee` (
-  `empid` int NOT NULL,
-  `firstname` varchar(45) NOT NULL,
-  `lastname` varchar(45) NOT NULL,
-  `contactno` int NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `loginid` int NOT NULL,
-  PRIMARY KEY (`empid`),
-  UNIQUE KEY `loginid_UNIQUE` (`loginid`),
-  KEY `loginid_idx` (`loginid`),
-  CONSTRAINT `loginid` FOREIGN KEY (`loginid`) REFERENCES `login` (`loginid`) ON DELETE CASCADE ON UPDATE CASCADE
+  `id` int NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email_id` varchar(45) NOT NULL,
+  `contact_no` int NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `login_id` int NOT NULL,
+  `desig_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login_id_UNIQUE` (`login_id`),
+  KEY `fkdesig_idx` (`desig_id`),
+  CONSTRAINT `fkdesig` FOREIGN KEY (`desig_id`) REFERENCES `designation` (`desig_id`),
+  CONSTRAINT `fklogin` FOREIGN KEY (`login_id`) REFERENCES `login` (`loginid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -44,7 +93,6 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (153667,'Vivek','P',9850,'viv@gmail.com',2),(162136,'Ganesh','K',9689,'gana@gmail.com',1);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,6 +108,7 @@ CREATE TABLE `login` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `roleid` int NOT NULL,
+  `active` int NOT NULL,
   PRIMARY KEY (`loginid`),
   KEY `roleid_idx` (`roleid`),
   CONSTRAINT `roleid` FOREIGN KEY (`roleid`) REFERENCES `role` (`roleid`)
@@ -72,8 +121,74 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES (1,'gana1234','Ganesh#123',1),(2,'vivk23','vivek@123',3);
+INSERT INTO `login` VALUES (1,'gana1234','Ganesh#123',1,0),(2,'vivk23','vivek@123',3,0);
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project` (
+  `id` int NOT NULL,
+  `project_title` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `created_date` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `created_by` int NOT NULL,
+  `assigned_to` int NOT NULL,
+  `client_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fkclient_id_idx` (`client_id`),
+  KEY `fkadmin_id_idx` (`created_by`),
+  KEY `fkmanager_id_idx` (`assigned_to`),
+  CONSTRAINT `fkadmin_id` FOREIGN KEY (`created_by`) REFERENCES `employee` (`id`),
+  CONSTRAINT `fkclient_id` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  CONSTRAINT `fkmanager_id` FOREIGN KEY (`assigned_to`) REFERENCES `employee` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `query`
+--
+
+DROP TABLE IF EXISTS `query`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `query` (
+  `qid` int NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `query_text` varchar(45) NOT NULL,
+  `created_text` varchar(45) NOT NULL,
+  `raised_by` int NOT NULL,
+  `task_id` int NOT NULL,
+  PRIMARY KEY (`qid`),
+  KEY `fkemployee_id_idx` (`raised_by`),
+  KEY `fktask_id_idx` (`task_id`),
+  CONSTRAINT `fkemp_id` FOREIGN KEY (`raised_by`) REFERENCES `employee` (`id`),
+  CONSTRAINT `fktask1_id` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `query`
+--
+
+LOCK TABLES `query` WRITE;
+/*!40000 ALTER TABLE `query` DISABLE KEYS */;
+/*!40000 ALTER TABLE `query` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -99,6 +214,123 @@ LOCK TABLES `role` WRITE;
 INSERT INTO `role` VALUES (1,'Manager'),(2,'Admin'),(3,'Associate');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `solution`
+--
+
+DROP TABLE IF EXISTS `solution`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solution` (
+  `id` int NOT NULL,
+  `completion_date` varchar(45) NOT NULL,
+  `solution_text` varchar(45) NOT NULL,
+  `solved_by` int NOT NULL,
+  `query_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fkemp_id1_idx` (`solved_by`),
+  KEY `fkquery_id_idx` (`query_id`),
+  CONSTRAINT `fkemp_id1` FOREIGN KEY (`solved_by`) REFERENCES `employee` (`id`),
+  CONSTRAINT `fkquery_id` FOREIGN KEY (`query_id`) REFERENCES `query` (`qid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solution`
+--
+
+LOCK TABLES `solution` WRITE;
+/*!40000 ALTER TABLE `solution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solution` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task` (
+  `task_id` int NOT NULL,
+  `created_date` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `project_id` int NOT NULL,
+  `assigned_to` int NOT NULL,
+  `start_date` varchar(45) NOT NULL,
+  `end_date` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`task_id`),
+  KEY `fkproject_id_idx` (`project_id`),
+  KEY `fkemployee_id_idx` (`assigned_to`),
+  CONSTRAINT `fkemployee_id` FOREIGN KEY (`assigned_to`) REFERENCES `employee` (`id`),
+  CONSTRAINT `fkproject_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_progress`
+--
+
+DROP TABLE IF EXISTS `task_progress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task_progress` (
+  `tid` int NOT NULL,
+  `update_date` varchar(45) NOT NULL,
+  `workdone_percent` float NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `task_id` int NOT NULL,
+  PRIMARY KEY (`tid`),
+  KEY `fktask_id_idx` (`task_id`),
+  CONSTRAINT `fktask_id` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_progress`
+--
+
+LOCK TABLES `task_progress` WRITE;
+/*!40000 ALTER TABLE `task_progress` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_progress` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team_member`
+--
+
+DROP TABLE IF EXISTS `team_member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `team_member` (
+  `employee_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  PRIMARY KEY (`employee_id`),
+  KEY `fkproject_id2_idx` (`project_id`),
+  CONSTRAINT `fkemp_id2` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
+  CONSTRAINT `fkproject_id2` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team_member`
+--
+
+LOCK TABLES `team_member` WRITE;
+/*!40000 ALTER TABLE `team_member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team_member` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -109,4 +341,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-02 18:50:45
+-- Dump completed on 2024-08-02 20:28:39
